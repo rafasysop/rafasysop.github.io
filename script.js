dfd78d79e1e16f3e54cda942ea4d904677e602c5
Rafael Moura <rafaelcostamoura@gmail.com>
2021-06-19 18:23:46 -0300

    inserindo loading

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
+
 const foto = document.querySelector('.foto')
 const descricao = document.querySelector('.descricao')
 const titleName = document.querySelector('.title-name')
 const subtitle = document.querySelector('.subtitle')
 const icons = document.querySelector('.icons')
 const main = document.querySelector('.main-container')
+const loading = document.querySelector('.loading')
 
 const tl = new TimelineMax();
 
 tl
+    .to(loading, .1, {display: 'none'})
     .from(foto, 1, {y: '-80vh', opacity: 0})
     .from(descricao, 1, {height: 0, padding: 0})
     .from(titleName, .9, { opacity: 0 })
